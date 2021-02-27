25bd7f56f853c6358bd5f5ebd9fc6886bf4e3630
Planet-Moon <topwastaken@abwesend.de>
2021-02-27 03:18:11 +0100

    [fix] fix package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="schroeda-telegrambot", # Replace with your own username
+    name="schroedaTelegrambot", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
